6cb0dad0e83020ba1e02454257a624945f595c89
JAY-Kittisak <71874439+JAY-Kittisak@users.noreply.github.com>
2021-03-12 17:04:55 +0700

    first commit

---
+++ Learn_Pandas.py
@@ -0,0 +1,16 @@
+import pandas as pd
+data_ls=[10,20,30,"test1","test2",True]
+data_ls
+# print(data_ls)
+pd.Series(data_ls)
+# print(pd.Series(data_ls)) 
+ps=pd.Series(data_ls)
+# print(ps) 
+
+data_tp=(10,20,30,"test1","test2",True)
+data_tp
+# print(data_tp)
+pd.Series(data_tp)
+# print(pd.Series(data_tp)) 
+ps=pd.Series(data_tp)
+# print(ps) 
